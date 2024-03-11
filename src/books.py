a216589ad67246baae2365faeb12ff6062760486
Thomas Kristian Lorentzen <thomas.lorentzen@hotmail.com>
2024-03-11 10:04:14 +0100

    :)

---
+++ src/books.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, title, author, release_year, unique_ISBN):
+        self.title = title
+        self.author = author
+        self.release_year = release_year
+        self.unique_ISBN = unique_ISBN
+
+    def is_available(self):
+        pass 
+
+    def reserve_book(self):
+        pass
---
+++ src/users.py
@@ -0,0 +1,14 @@
+
+class User:
+    def __init__(self, name, user_id, borrowed_books):
+        self.name = name
+        self.user_id = user_id
+        self.borrowed_books = borrowed_books
+
+    def borrow_book(self, book):
+        pass
+
+    def return_book(self, book):
+        pass
+
+        
