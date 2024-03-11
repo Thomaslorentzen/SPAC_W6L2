c4e3dc94ef9e98c5aeb589deb27782d2e71e9592
Thomas Kristian Lorentzen <thomas.lorentzen@hotmail.com>
2024-03-11 10:54:24 +0100

    :D

--- src/books.py
+++ src/books.py
@@ -4,9 +4,31 @@ class Book:
         self.author = author
         self.release_year = release_year
         self.unique_ISBN = unique_ISBN
+        self.available = True
+        self.reserved = None
+
 
     def is_available(self):
-        pass 
+        return self.available
 
-    def reserve_book(self):
-        pass
+    def reserve_book(self, user_id):
+        if self.available:
+            self.available = False
+            self.reserved_by = user_id
+            return True
+        else:
+            return False
+        
+    
+    def loan_book(self, user_id):
+        if self.available:
+            self.available = True
+            self.reserved_by = user_id
+            return True
+        else:
+            return False
+        
+    def return_book(self):
+        self.available = True
+        self.reserved_by = None
+        return True
