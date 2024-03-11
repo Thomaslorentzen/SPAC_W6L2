89f40a60621193322636f48c56d308faed9425cc
Thomas Kristian Lorentzen <thomas.lorentzen@hotmail.com>
2024-03-11 13:10:09 +0100

    refaktoreret til at bruge decorators og sqlalchemy

--- src/users.py
+++ src/users.py
@@ -1,10 +1,19 @@
 from books import Book
 
-class User:
-    def __init__(self, name, user_id):
-        self.name = name
-        self.user_id = user_id
-        self.borrowed_books = []
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    user_id = Column(String, unique=True)
+
+    borrowed_books = relationship("Book", secondary='borrowed_books')
 
     def borrow_book(self, book):
         if book.is_available():
@@ -25,3 +34,12 @@ class User:
                 return False  # Error in returning book
         else:
             return False  # User hasn't borrowed this book
+
+class BorrowedBooks(Base):
+    __tablename__ = 'borrowed_books'
+
+    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    book_id = Column(Integer, ForeignKey('books.id'), primary_key=True)
+
+    user = relationship(User, backref='borrowed_books')
+    book = relationship("Book", backref='borrowed_by')
