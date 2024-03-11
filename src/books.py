823a1aebc1f03688a8a7039364062028198da752
Søren <soeren@langkilde.com>
2024-03-11 13:45:55 +0100

    Temp changes to experiment with sqlalchemy

--- src/books.py
+++ src/books.py
@@ -1,4 +1,20 @@
-class Book:
+from sqlalchemy import Column, Integer, String, ForeignKey, BigInteger
+from sqlalchemy.orm import relationship
+from src.constants import Base
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    unique_ISBN = Column(String(255), primary_key=True, autoincrement=False)
+    title = Column(String(50))
+    author = Column(String(50))
+    publish_year = Column(Integer)
+    user_id = Column(
+        Integer, ForeignKey("users.id")
+    )  # Foreign key to link to users table
+    user = relationship("User", back_populates="books")
+
     def __init__(self, title, author, release_year, unique_ISBN):
         self.title = title
         self.author = author
@@ -6,7 +22,6 @@ class Book:
         self.unique_ISBN = unique_ISBN
         self.available = True
         self.reserved = None
-
 
     def is_available(self):
         return self.available
@@ -18,8 +33,7 @@ class Book:
             return True
         else:
             return False
-        
-    
+
     def loan_book(self, user_id):
         if self.available:
             self.available = True
@@ -27,7 +41,7 @@ class Book:
             return True
         else:
             return False
-        
+
     def return_book(self):
         self.available = True
         self.reserved_by = None
--- src/users.py
+++ src/users.py
@@ -1,6 +1,17 @@
-from books import Book
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
-class User:
+from src.books import Book
+from src.constants import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    books = relationship("Book", back_populates="user")
+
     def __init__(self, name, user_id):
         self.name = name
         self.user_id = user_id
